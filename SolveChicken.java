bf7ae5636c72cb9fe528806ac908c1cc3b1f31c2
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-07 21:24:32 -0500

    Add files via upload

--- SolveChicken.java
+++ SolveChicken.java
@@ -1,2 +1,31 @@
 public class SolveChicken {
+    public static void main(String[] args) {
+        Hen hen = HenFactory.getHen(Continent.AFRICA);
+        hen.getMonthlyEggCount();
+    }
+
+    static class HenFactory {
+
+        static Hen getHen(String continent) {
+            Hen hen = null;
+
+            switch (continent) {
+                case Continent.AFRICA:
+                    hen = new AfricanHen();
+                    break;
+                case Continent.NORTHAMERICA:
+                    hen = new NorthAmericanHen();
+                    break;
+                case Continent.EUROPE:
+                    hen = new EuropeanHen();
+                    break;
+                case Continent.ASIA:
+                    hen = new AsianHen();
+                    break;
+            }
+            return hen;
+        }
+    }
 }
+
+
