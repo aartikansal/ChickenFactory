ac64146bdd0a3b17494c927ea8f55f7ed6450aaa
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-07 21:29:24 -0500

    Add files via upload

---
+++ EuropeanHen.java
@@ -0,0 +1,11 @@
+public class EuropeanHen extends Hen {
+    @Override
+    String getDescription() {
+        return super.getDescription() + String.format(" I come from %s. I lay %d eggs a month.", Continent.NORTHAMERICA, getMonthlyEggCount());
+    }
+
+
+    int getMonthlyEggCount() {
+        return 20;
+    }
+}
